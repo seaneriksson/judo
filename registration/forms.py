d85528e20ef8a0e8919d0e7ada2340426867e660
Sean Eriksson <seaneriksson@gmail.com>
2018-12-31 15:22:45 -0500

    added post form for registration

---
+++ registration/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import Registration
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Registration
+        fields = ('firstName', 'lastName',)
--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .forms import PostForm
+
 
 # Create your views here.
 def post_list(request):
     return render(request, 'registration/post_list.html', {})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return render(request, 'registration/post_edit.html', {'form': form})
+            ##print success 
+    else:
+        form = PostForm()
+    return render(request, 'registration/post_edit.html', {'form': form})
