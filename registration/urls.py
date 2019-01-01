6fc44c98686b732aa2fcdf855719710132a6a7aa
Sean Eriksson <seaneriksson@gmail.com>
2019-01-01 16:44:06 -0500

    updated urls.py

--- registration/urls.py
+++ registration/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('registration/new', views.post_new, name='post_new'),
+    path('registration/new/', views.post_new, name='post_new'),
 ]
